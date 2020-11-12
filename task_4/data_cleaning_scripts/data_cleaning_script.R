0aae9a82c3dc9e14e8dec51ea7785baeddb8e3e1
GeraldineSmith20 <geraldine.smith3010@gmail.com>
2020-11-12 14:45:53 +0000

    Afternoon backup of dirty data task 4

--- task_4/data_cleaning_scripts/data_cleaning_script.R
+++ task_4/data_cleaning_scripts/data_cleaning_script.R
@@ -6,13 +6,13 @@ library(readxl)
 
 #---------------------read in data---------------------------------------------
 
-candy_2015 <- read_xlsx("../raw_data/boing-boing-candy-2015.xlsx") %>% 
+candy_2015 <- read_xlsx("../task_4/raw_data/boing-boing-candy-2015.xlsx") %>% 
   select(c("Timestamp":"[York Peppermint Patties]"))
 
-candy_2016 <- read_xlsx("../raw_data/boing-boing-candy-2016.xlsx") %>% 
+candy_2016 <- read_xlsx("../task_4/raw_data/boing-boing-candy-2016.xlsx") %>% 
   select(c("Timestamp":"[York Peppermint Patties]"))
 
-candy_2017 <- read_xlsx("../raw_data/boing-boing-candy-2017.xlsx") %>% 
+candy_2017 <- read_xlsx("../task_4/raw_data/boing-boing-candy-2017.xlsx") %>% 
   select(c("Internal ID":"Q6 | York Peppermint Patties"))
 
 # 2015 data - fixing column names, adding required structure for merging with
@@ -51,7 +51,7 @@ candy_2016 <- clean_names(candy_2016) %>%
          year = 2016) %>%
   
   rename(trick_or_treating =
-           are_you_going_actually_going_trick_or_treating_yourself,
+         are_you_going_actually_going_trick_or_treating_yourself,
          gender = your_gender,
          age = how_old_are_you,
          country = 
@@ -104,14 +104,119 @@ candy_2017_tidy <- candy_2017 %>%
 
 all_candy_data <- bind_rows(candy_2015_tidy, candy_2016_tidy, candy_2017_tidy)
 
+# extract age values as those with 2 numbers, then replace N/A's with --------
+# median of age
 
+all_candy_data <- all_candy_data %>% 
+  mutate(age = str_extract(age, pattern = "[0-9]{2}")) %>%
+  mutate(age = as.numeric(age)) %>% 
+  mutate(age = coalesce(age, median(age, na.rm = TRUE)))
 
+# Replacing values in the gender column to reduce no. of categories-------------
 
+all_candy_data <- all_candy_data %>% 
+  mutate(
+    gender = if_else(
+      gender == "I'd rather not say", "Undisclosed",
+      gender
+    )
+  ) %>% 
+  mutate(gender = replace_na(gender, "Unknown")) %>% 
+  mutate(gender = if_else(
+    gender == "unknown", "Unknown",
+    gender
+  )) 
+
+# replacing NA's in the trick or treating column with "Unknown"-----------------
+all_candy_data <- all_candy_data %>% 
+  mutate(trick_or_treating = replace_na(trick_or_treating, "Unknown"))
+
+#cleaning the country column the hard way - PART 1------------------------------
+
+all_candy_data <- all_candy_data %>% 
+  mutate(country_clean = tolower(country)) %>% 
+  mutate(country_clean = case_when(
+    startsWith(country_clean,"uni") ~ "usa",
+    startsWith(country_clean, "usa") ~ "usa",
+    startsWith(country_clean, "u.") ~ "usa",
+    startsWith(country_clean, "us") ~ "usa",
+    startsWith(country_clean, "u s") ~ "usa",
+    startsWith(country_clean, "trump") ~ "usa",
+    startsWith(country_clean, "mur") ~ "usa",
+    startsWith(country_clean, "new") ~ "usa",
+    startsWith(country_clean, "north carolina") ~ "usa",
+    startsWith(country_clean, "pitts") ~ "usa",
+    startsWith(country_clean, "the netherlands") ~ "netherlands",
+    startsWith(country_clean, "god") ~ "unknown",
+    startsWith(country_clean, "not the") ~ "unknown",
+    startsWith(country_clean, "espa") ~ "spain",
+    TRUE ~ as.character(country_clean))) %>% 
+  mutate(country_clean = case_when(
+    endsWith(country_clean, "usa") ~ "usa",
+    endsWith(country_clean, "sa!") ~ "usa",
+    endsWith(country_clean, "ates") ~ "usa",
+    endsWith(country_clean, "merica") ~ "usa",
+    endsWith(country_clean, "stetes") ~ "usa",
+    endsWith(country_clean, "ayy") ~ "usa",
+    endsWith(country_clean, "tates.") ~ "usa",
+    endsWith(country_clean, "yyy") ~ "usa",
+    endsWith(country_clean, "merca") ~ "usa",
+    endsWith(country_clean, "tates") ~ "usa",
+    endsWith(country_clean, "alaska") ~ "usa",
+    endsWith(country_clean, "england") ~ "uk",
+    TRUE ~ as.character(country_clean))) 
+
+#cleaning the country column - the hard way PART 2------------------------------
+
+all_candy_data <- all_candy_data %>% 
+  mutate(country_clean = case_when(
+    endsWith(country_clean, "6") ~ "unknown",
+    endsWith(country_clean, "2") ~ "unknown",
+    endsWith(country_clean, "lately") ~ "unknown",
+    endsWith(country_clean, "ud") ~ "unknown",
+    endsWith(country_clean, "tube") ~ "unknown",
+    endsWith(country_clean, "loathing") ~ "unknown",
+    endsWith(country_clean, "anymore") ~ "unknown",
+    endsWith(country_clean, "1") ~ "unknown",
+    TRUE ~ as.character(country_clean))) 
+
+#cleaning the country column - the hard way PART 3------------------------------
+
+all_candy_data <- all_candy_data %>% 
+  mutate(country_clean = case_when(
+    endsWith(country_clean, "equator") ~ "unknown",
+    endsWith(country_clean, " one") ~ "unknown",
+    endsWith(country_clean, "ones") ~ "unknown",
+    endsWith(country_clean, "old men") ~ "unknown",
+    endsWith(country_clean, ".0") ~ "unknown",
+    endsWith(country_clean, "where") ~ "unknown",
+    endsWith(country_clean, "see above") ~ "unknown",
+    endsWith(country_clean, "5") ~ "unknown",
+    endsWith(country_clean, "scotland") ~ "uk",
+    TRUE ~ as.character(country_clean))) 
+
+# clean candy_type column to reduce distinct variables-------------------------
+
+all_candy_data <- all_candy_data %>% 
+  mutate(candy_type = str_remove(candy_type, pattern = "q6_")) %>% 
+  mutate(candy_type = str_remove(candy_type, pattern = "_a_k_a")) %>% 
+  mutate(candy_type = str_remove(candy_type, pattern = "_a_friend_to_diabetes")) %>%
+  mutate(candy_type = str_remove(candy_type, pattern = "_mary_janes")) %>% 
+  mutate(candy_type = case_when(
+    startsWith(candy_type, "x") ~ "100_grand_bar",
+    startsWith(candy_type, "boxo") ~ "box_o_raisins",
+    TRUE ~ as.character(candy_type)
+  ))
+
+# force lower case on the candy_feelings column---------------------------------
+
+all_candy_data <- all_candy_data %>% 
+  mutate(candy_feelings = str_to_lower(candy_feelings))
 
 
 #----------write cleaned, combined data set to csv file------------------------
 
-write_csv(all_candy_data, file = "../clean_data/cleaned_candy_data")
+write_csv(all_candy_data, file = "../task_4/clean_data/cleaned_candy_data")
 
 
 
